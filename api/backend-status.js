e1b8ecf5492ef7e83f289ed9676bf329eff152e4
scostavanessa-jpg <scosta.vanessa@gmail.com>
2026-07-28 22:51:47 -0300

    chore: remove temporary backend status probe

--- api/backend-status.js
+++
@@ -1,8 +0,0 @@
-export default function handler(_req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  return res.status(200).json({
-    databaseUrl: Boolean(process.env.DATABASE_URL),
-    neonAuthBaseUrl: Boolean(process.env.NEON_AUTH_BASE_URL || process.env.VITE_NEON_AUTH_URL),
-    neonDataApiUrl: Boolean(process.env.NEON_DATA_API_URL || process.env.VITE_NEON_DATA_API_URL),
-  });
-}
